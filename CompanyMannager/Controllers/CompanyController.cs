2127a94af7fd9e7b1b90864fcf988507300ec5a9
Maciej Scholz <maciej-scholz@tlen.pl>
2020-11-27 14:34:17 +0100

    Added base structure

---
+++ CompanyMannager/Controllers/CompanyController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CompanyMannager.Controllers
+{
+    [ApiController]
+	public class CompanyController : ControllerBase
+	{
+		private readonly ILogger<CompanyController> _logger;
+
+		public CompanyController(ILogger<CompanyController> logger)
+		{
+			_logger = logger;
+		}
+	}
+}
